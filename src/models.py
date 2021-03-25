11779ef763b3c412b4c2027fd03617492a234608
fabianchs <fabichasola@hotmail.com>
2021-03-25 03:07:27 +0000

    avance en general

--- src/models.py
+++ src/models.py
@@ -8,12 +8,8 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+#python src/models.py
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -22,6 +18,23 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
+
+class FavPeople(Base):
+    __tablename__ = 'FavPeople'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, primary_key=True)
+    people_id= Column(Integer, primary_key=True)
+
+
+
+class Person(Base):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
