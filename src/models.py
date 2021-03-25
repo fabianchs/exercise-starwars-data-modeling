994d0aa898ebff1b150db67372af3fdc25444866
fabianchs <fabichasola@hotmail.com>
2021-03-25 02:55:01 +0000

    avance inicial

--- src/models.py
+++ src/models.py
@@ -15,6 +15,14 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
